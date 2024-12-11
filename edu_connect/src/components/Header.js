ad9ae6d491388415cda272e74a747a15a305a21f
Calm-the-loony <valeriya.p05@mail.ru>
2024-12-11 10:48:57 +0300

    commit

--- edu_connect/src/components/Header.js
+++ edu_connect/src/components/Header.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Header.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Header.css";
 
 function Header() {
+  const location = useLocation(); // Получение текущего пути
+
+  const isActive = (path) => location.pathname === path; // Проверка активности
+
   return (
     <header className="header">
       <div className="container">
         <nav>
           <ul className="header__nav">
             <li>
-              <Link to="/" className="header__link_active">главная</Link>
+              <Link
+                to="/"
+                className={`header__link ${isActive("/") ? "header__link_active" : ""}`}
+              >
+                главная
+              </Link>
             </li>
             <li>
-              <Link to="/profile" className="header__link">мой профиль</Link>
+              <Link
+                to="/profile"
+                className={`header__link ${isActive("/profile") ? "header__link_active" : ""}`}
+              >
+                мой профиль
+              </Link>
             </li>
             <li>
-              <Link to="/vacancies" className="header__link">вакансии</Link>
+              <Link
+                to="/vacancies"
+                className={`header__link ${isActive("/vacancies") ? "header__link_active" : ""}`}
+              >
+                вакансии
+              </Link>
             </li>
             <li>
-              <Link to="/courses" className="header__link">курсы</Link>
+              <Link
+                to="/courses"
+                className={`header__link ${isActive("/courses") ? "header__link_active" : ""}`}
+              >
+                курсы
+              </Link>
             </li>
             <li>
-              <Link to="/forum" className="header__link">форум</Link>
+              <Link
+                to="/forum"
+                className={`header__link ${isActive("/forum") ? "header__link_active" : ""}`}
+              >
+                форум
+              </Link>
             </li>
           </ul>
         </nav>
